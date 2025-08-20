da5e776b12f5164fac1618de881c27c87c83daff
maciek100 <maciek100@gmail.com>
2025-08-20 11:51:28 -0500

    making sure that the cloud momgodb 'secrets' are not leaked to github.

--- src/main/java/urlShortener/service/ScheduledTasks.java
+++ src/main/java/urlShortener/service/ScheduledTasks.java
@@ -44,7 +44,7 @@ public class ScheduledTasks {
 
     //@Scheduled(fixedRate = 60000000) // every 60s
     public void markExpiredRecords() {
-        logger.info("HF marking expired records should be OFF");
+        logger.warn("Marking expired records should be OFF");
         Instant now = Instant.now();
         shortUrlRepository.findByExpiresAtBeforeAndExpiredFalse(now)
                 .flatMap(record -> {
@@ -57,16 +57,16 @@ public class ScheduledTasks {
     // runs every 5 minutes
     @Scheduled(fixedRate = 5 * 60 * 1000)
     public void expireRandomUrls() {
-        logger.info("This MF, expire random urls is OK");
+        logger.info("Expire random urls is OK");
         statisticsService.expireRandomUrlRecords(10)
                 .doOnTerminate(() -> logger.info("Finished expiring records"))
                 .subscribe(
                         summary -> {
-                            System.out.println("===== ELIMINATION ROUND =====");
+                            System.out.println("===== ELIMINATION ROUND RESULTS =====");
                             System.out.println("Picked: " + summary.get("pickedIds"));
                             System.out.println("Already expired: " + summary.get("alreadyExpired"));
                             System.out.println("Newly expired: " + summary.get("newlyExpired"));
-                            System.out.println("=============================");
+                            System.out.println("============= END ================");
                         },
                         error -> logger.warn("Error expiring URLs: {}", error.getMessage())
                 );
